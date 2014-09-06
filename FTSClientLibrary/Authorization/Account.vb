b5d9a49462c0bad986a53c965498ae78c487d8db
SamLangTen <samlangten@outlook.com>
2014-09-07 00:01:48 +0800

    添加：连接设置类和账号类

---
+++ FTSClientLibrary/Authorization/Account.vb
@@ -0,0 +1,37 @@
+﻿Imports Newtonsoft.Json
+Imports System.IO
+Imports System.Text
+Imports System.Net
+
+Namespace Authorization
+    ''' <summary>
+    ''' 表示一个FTS的用户
+    ''' </summary>
+    Public Class Account
+#Region "Private Members"
+
+#End Region
+#Region "Public Members"
+
+        ''' <summary>
+        ''' 登录到FailureTroubleShooting
+        ''' </summary>
+        ''' <param name="Password"></param>
+        ''' <param name="RememberMe"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Function Login(Password As String, RememberMe As Boolean)
+
+        End Function
+
+        Public Property Id As Integer
+        Public Property UserName As String
+        Public Property DisplayName As String
+        Public Property Grade As Integer
+        Public Property [Class] As Integer
+        Public Property Roles As String()
+
+
+#End Region
+    End Class
+End Namespace
---
+++ FTSClientLibrary/Connection/ConnectionSetting.vb
@@ -0,0 +1,12 @@
+﻿
+Namespace Connection
+    ''' <summary>
+    ''' 用于连接到FailureTroubleShooting服务器的连接设置文件
+    ''' </summary>
+    Public Class ConnectionSetting
+        ''' <summary>
+        ''' 表示要连接到的FailureTroubleShooting服务器的主域名
+        ''' </summary>
+        Public Shared Property DomainName As String
+    End Class
+End Namespace
